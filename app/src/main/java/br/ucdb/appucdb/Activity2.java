439bad2b652580d4b675d8f037066648cdd543e1
virmerson <virmerson@hightechcursos.com.br>
2015-08-12 20:42:18 -0400

    Parametros Extras entre intents

--- app/src/main/java/br/ucdb/appucdb/Activity2.java
+++ app/src/main/java/br/ucdb/appucdb/Activity2.java
@@ -1,9 +1,12 @@
 package br.ucdb.appucdb;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Virmerson on 05/08/15.
@@ -16,9 +19,24 @@ public class Activity2 extends AppCompatActivity {
         setContentView(R.layout.activity_2);
 
         Button btnVoltar = (Button) findViewById(R.id.btn_voltar);
+        //TextView tvNome = (TextView) findViewById(R.id.tv_nome);
+       final EditText edNomeTela2 = (EditText) findViewById(R.id.ed_nome_tela2);
+
+        //Acessando a intent que chamou esta Activity
+        Intent it = getIntent();
+        //Acessando os parametros extras
+        Bundle parametrosExtras = it.getExtras();
+        String nome = parametrosExtras.getString("nome");
+        //Preenchendo o componente EditText com o nome vindo no parametro
+        edNomeTela2.setText(nome);
+
+
         btnVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent i = new Intent();
+                i.putExtra("nome", edNomeTela2.getText().toString());
+                setResult(RESULT_OK,i);
                 finish();
             }
         });
--- app/src/main/java/br/ucdb/appucdb/MainActivity.java
+++ app/src/main/java/br/ucdb/appucdb/MainActivity.java
@@ -14,6 +14,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int REQUEST_TELA2 =1 ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,10 +56,43 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //Carregando tela 2
                 Intent irParaTela2 = new Intent(MainActivity.this, Activity2.class);
-                startActivity(irParaTela2);
+                //Lendo texto do componente EditText
+                String texto = edNome.getText().toString();
+
+                //Criando um "bando" de parametros
+                Bundle parametrosExtras = new Bundle();
+                parametrosExtras.putString("nome", texto);
+
+                irParaTela2.putExtras(parametrosExtras);
+
+               // startActivity(irParaTela2);
+
+                startActivityForResult(irParaTela2, REQUEST_TELA2);
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Se a resposta for do REQUEST_TELA2 e deu tudo certo
+        if(requestCode== REQUEST_TELA2 && resultCode==RESULT_OK){
+           Bundle parametrosExtrasResult = data.getExtras();
+           String nome =  parametrosExtrasResult.getString("nome");
+            final EditText edNome = (EditText) findViewById(R.id.ed_nome);
+            edNome.setText(nome);
+
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Acessa o campo de texto
+        //final EditText edNome = (EditText) findViewById(R.id.ed_nome);
+        //edNome.setText("");
+
+    }
 }
 
